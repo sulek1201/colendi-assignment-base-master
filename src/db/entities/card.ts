47b70bf971cbf9a7af897bf2f166d360e79c3074
sulek1201 <aanil1201@gmail.com>
2022-01-02 17:48:28 +0300

    Added Missing Entities

--- src/db/entities/card.ts
+++ src/db/entities/card.ts
@@ -1,3 +1,38 @@
-/**
- * @todo will be implemented
- */
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne} from 'typeorm';
+
+
+import { BaseEntity } from './baseEntity';
+import {Gpa} from "@entities/gpa";
+
+
+
+@Entity('card')
+export class Card extends BaseEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    @Column()
+    token: string;
+
+    @Column({ unique: true })
+    gpaId: string;
+
+    @OneToOne(() => Gpa)
+    gpa: Gpa;
+
+    @Column({ nullable: false })
+    pan: string;
+
+    @Column({ nullable: false })
+    cvv: string;
+
+    @Column({ nullable: false })
+    expiresAt: string;
+
+    @Column({ nullable: false})
+    status: boolean;
+
+    @Column({ nullable: true})
+    allowedMccs: boolean;
+
+
+}
--- src/db/entities/cardTransaction.ts
+++ src/db/entities/cardTransaction.ts
@@ -1,3 +1,29 @@
-/**
- * @todo will be implemented
- */
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne, Long} from 'typeorm';
+
+
+import { BaseEntity } from './baseEntity';
+import {Merchant} from "@entities/merchant";
+import {Gpa} from "@entities/gpa";
+
+
+
+@Entity('card_transactions')
+export class CardTransaction extends BaseEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    @Column()
+    id: string;
+
+    @Column()
+    merchantId: string;
+
+    @OneToOne(() => Gpa)
+    gpa: Merchant;
+
+    @Column({ nullable: false })
+    cardToken: string;
+
+    @Column()
+    amount: Long;
+
+}
